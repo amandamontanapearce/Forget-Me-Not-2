8f614144bc30d2a1186c4d6f1ab697f128a43d25
Amanda Montana Pearce <amandamontanapearce@gmail.com>
2017-10-16 16:50:46 -0600

    mysql database init, add mysqladmin for ease of use

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,6 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
-                <example-component></example-component>
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
