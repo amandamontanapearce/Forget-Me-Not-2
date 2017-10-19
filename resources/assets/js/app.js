85ac40d30f16f678e0e8b6d86057eee2cb82d172
Amanda Montana Pearce <amandamontanapearce@gmail.com>
2017-10-18 23:05:33 -0600

    create develop branch and begin scaffolding of add-item vue component

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -16,6 +16,7 @@ window.Vue = require('vue');
  */
 
 Vue.component('example', require('./components/Example.vue'));
+Vue.component('add-item', require('./components/list/add-item.vue'));
 
 const app = new Vue({
     el: '#app'
---
+++ resources/assets/js/components/list/add-item.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="container">
+  <div class="row">
+    <div class="col-12">
+      <div class="card card-body bg-light">
+        <div class="card-title">Add a task...</div>
+        <div class="card-body">
+          <div class="form-group">
+            <label for="title" class="form-group-label">Task</label>
+            <input type="text" name="title" v-model="task.title">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+<script>
+export default {
+  props: [],
+  data() {
+    return {
+      task: {
+        title: '',
+      },
+    }
+  },
+
+
+}
+<script>
